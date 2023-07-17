c1a371111e4584ee857683d2e8c34c5ca0f2181c
Sean Xiong <seanxiong@Seans-MBP.hitronhub.home>
2023-07-17 15:14:18 -0700

    July 17th 2023

---
+++ base/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('room/<int:pk>/', views.room, name = 'room'),
+    path('create-room/', views.createRoom, name = 'create-room'),
+    path('update-room/<int:pk>', views.updateRoom, name = 'update-room'),
+    path('delete-room/<int:pk>', views.deleteRoom, name = 'delete-room'),
+
+]
